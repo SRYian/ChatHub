3ada2dfea8f2a8a2fc213d7a9a276a73b59a54db
SRYian <92420947+SRYian@users.noreply.github.com>
2022-11-23 11:47:45 +0700

    feat: add message sync

--- ChatHub/wwwroot/js/chat.js
+++ ChatHub/wwwroot/js/chat.js
@@ -7,7 +7,10 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
+    var h5 = document.createElement("h5");
+    var msgcontainer = document.createElement("div");
+    var msg = document.createElement("div");
+    var timestamp = document.createElement("small")
     var date;
     date = new Date();
     date = ('00' + date.getUTCDate()).slice(-2)+ '/' +
@@ -17,8 +20,18 @@ connection.on("ReceiveMessage", function (user, message) {
         ('00' + date.getUTCMinutes()).slice(-2) + ':' +
         ('00' + date.getUTCSeconds()).slice(-2);
     console.log("date: " + date)
-
-    li.textContent = `${globalname} ${message} ${date}`;
+    li.appendChild(h5);
+    
+    document.getElementById("messagesList").appendChild(li);
+    console.log(li);
+    //h5.textContent = `${user} ${message} ${date}`;
+    h5.textContent = `${user}`;
+    li.appendChild(msgcontainer);
+     msgcontainer.appendChild(msg);
+     msg.textContent = `${message}`;
+     msgcontainer.appendChild(timestamp);
+     timestamp.textContent = `${date}`;
+    li.classList.add("msglist");
 });
 
 connection.start().then(function () {
@@ -28,16 +41,17 @@ connection.start().then(function () {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
+    //var user = document.getElementById("userInput").value;
+    var user = globalname;
     var message = document.getElementById("messageInput").value;
     var data = {
-        userid: user,
+        userid: globalname,
         text: message
     };
 
     
     
-    $.post("/Chat/Create", { user, message }, function (err) { console.log(user + " " + message + " " + err) });
+    $.post("/Chat/Create", { user, message }, function () { console.log(user + " " + message) });
 
     //configure endpoint here
     connection.invoke("SendMessage", user, message).catch(function (err) {
